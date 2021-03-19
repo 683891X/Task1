bb3f9d7cd63dc481351987631e5556805221d8f6
683891X <683891X@student.swin.edu.au>
2021-03-19 17:19:45 +1100

    Exercise 1.1

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+
+            Console.WriteLine("Please enter your age");
+            age = int.Parse(Console.ReadLine());
+
+            if(age  > 17){
+                Console.WriteLine("Legally adult age");
+            }else{
+                Console.WriteLine("Legally not adult age");
+            }
+        }   
+    }
+}
